4f05216f8ad1e0567dcf3836ed1887f51aabb072
kanishkkasri g <157386788+01fe22bca059@users.noreply.github.com>
2024-05-14 10:37:38 +0530

    Update ExtendedCalculator.java

--- ExtendedCalculator.java
+++ ExtendedCalculator.java
@@ -1,7 +1,1 @@
-package J2;
 
-class ExtendedCalculator {
-    public static double squareRoot(double num) {
-        return Math.sqrt(num);}
-    public static double cubeRoot(double num) {
-        return Math.cbrt(num);}}
