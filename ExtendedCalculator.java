4817bb9748a1de82095bed6c66d0a1dfc94f4c16
kanis <01e>
2024-05-14 08:29:06 +0530

    dos

---
+++ ExtendedCalculator.java
@@ -0,0 +1,7 @@
+package J2;
+
+class ExtendedCalculator {
+    public static double squareRoot(double num) {
+        return Math.sqrt(num);}
+    public static double cubeRoot(double num) {
+        return Math.cbrt(num);}}
